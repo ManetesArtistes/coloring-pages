b6a987191f97f4c7783a0fc6e654087c794f1f8d
j <anonymosmail@gmail.com>
2024-10-24 19:38:28 +0200

    add draws json

---
+++ app/src/main/java/com/example/manetesartistes_game/activities/ImageList.kt
@@ -0,0 +1,14 @@
+package com.example.manetesartistes_game.activities
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ImageList: AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
